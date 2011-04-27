40b1ee9ac7bed6ef2385e2d074609490312755f3
smithwick32 <davesieh@gmail.com>
2011-04-27 14:00:33 -0700

    Added a comment about how much I like this file.

--- tcmd.rb
+++ tcmd.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/env ruby
+# I don't this this particular script has a whole lot of value. Unless
+# of course, you want to output the first two arguments on the command
+# line, then output the entire command line twice.
+#
+# I, for one, think this is awesome.
+#
 puts "$0 - #{$0}"
 puts "$1 - #{$1}"
 puts "ARGV - #{ARGV}"
